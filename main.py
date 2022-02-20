13392685eb732bd4e6bcede2d658a6c535b9c0f6
BrunoGupa <bruno.gupa@gmail.com>
2022-02-20 17:09:19 -0600

    recursive function added

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ nato_alph = pd.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dic = {row.letter: row.code for (index, row) in nato_alph.iterrows()}
 
-word = input("Type a word: ")
-my_list = [letter for letter in word]
-try:
-    phonetic_list = [nato_dic[letter.upper()] for letter in word]
-except KeyError:
-    print("Sorry. Nato Alphabet only accepts letters")
-else:
-    print(phonetic_list)
 
+def generate_input():
+    word = input("Type a word: ")
+    my_list = [letter for letter in word]
+    try:
+        phonetic_list = [nato_dic[letter.upper()] for letter in word]
+    except KeyError:
+        print("Sorry. Nato Alphabet only accepts letters")
+        generate_input()
+    else:
+        print(phonetic_list)
+
+
+generate_input()
