f16c512a1f12fb1d76c667631c107834d60583a4
BrunoGupa <bruno.gupa@gmail.com>
2022-02-20 17:04:03 -0600

    we add exception when numbers are inserted

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ nato_alph = pd.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dic = {row.letter: row.code for (index, row) in nato_alph.iterrows()}
 
-word = input("Type a word ")
+word = input("Type a word: ")
 my_list = [letter for letter in word]
-phonetic_list = [nato_dic[letter.upper()] for letter in word]
-print(phonetic_list)
+try:
+    phonetic_list = [nato_dic[letter.upper()] for letter in word]
+except KeyError:
+    print("Sorry. Nato Alphabet only accepts letters")
+else:
+    print(phonetic_list)
 
